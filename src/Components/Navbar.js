bc949e7f3e3653dbedbfaecf97b7bdaf58ebc980
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-08-09 15:35:47 +0500

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,26 @@
 import React from "react";
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
-
+// import app.css
+import "../App.css";
 export default function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg">
+      <div className="collapse navbar-collapse" id="navbarText">
         <h3>Pizza Shop</h3>
 
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+        <ul className="navbar-nav ml-auto ">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Features
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
