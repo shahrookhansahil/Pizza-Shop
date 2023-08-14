3685e1e04548a4563c13c7248bbc467d6abb4a5f
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-08-14 10:48:50 +0500

    updated

--- src/Components/Pizza.js
+++ src/Components/Pizza.js
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
-
+import { Modal } from "react-bootstrap";
 export default function Pizza({ pizza }) {
   const [quantity, setQuantity] = useState(1);
   const [varient, setVarient] = useState("small");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div
       style={{ margin: " 90px" }}
       className="shadow-lg p-3 mb-5 bg-body rounded"
     >
-      <h1>{pizza.name}</h1>
-      <img
-        src={pizza.image}
-        alt={pizza.name}
-        className="img-fluid"
-        style={{ height: "250px", width: "250px" }}
-      />
+      <div onClick={handleShow}>
+        <h1 className="mt-4 mb-1">{pizza.name}</h1>
+        <img
+          src={pizza.image}
+          alt={pizza.name}
+          className="img-fluid"
+          style={{ height: "250px", width: "250px" }}
+        />
+      </div>
 
       <div className="card-select">
-        <div className="w-100 m-1">
+        <div className="w-100 m-1 mt-4">
           <p>Varient</p>
           <select
             className="form-control"
@@ -32,7 +38,7 @@ export default function Pizza({ pizza }) {
             })}
           </select>
         </div>
-        <div className="w-100 m-1">
+        <div className="w-100 m-1 mt-4">
           <p>Quantity</p>
           <select
             className="form-control"
@@ -58,6 +64,21 @@ export default function Pizza({ pizza }) {
           <button className="btn btn-danger">Add to Cart</button>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={pizza.image} alt={pizza.name} className="img-fluid w-100" />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button variant="secondary" onClick={handleClose}>
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
